8702764c08768eaf4be33e635cc9207808aab7d5
EcheCostabel <exequielcostabel@gmail.com>
2023-01-31 16:30:36 -0300

    adding routes

--- src/App.js
+++ src/App.js
@@ -2,9 +2,21 @@ import { BrowserRouter as Router, Route, Switch, Routes } from "react-router-dom
 
 
 function App() {
-  return (
-    <div className="App">
+  const user = null;
 
+  return (
+    <div className={classes.root}>
+      <Router>
+        {
+          !user ? (<Login />) : (
+            <Routes>
+              <Route path="/profile" element={<Profile />}></Route>
+              <Route path="/checkout" element={<Paypal />}></Route>
+              <Route path="/" element={<Home />}></Route>
+            </Routes>
+          )
+        }
+      </Router>
     </div>
   );
 }
