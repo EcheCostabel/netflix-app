a435088b3827ba311c91b8f598b1ac86034c6db6
EcheCostabel <exequielcostabel@gmail.com>
2023-02-01 17:49:35 -0300

    finishing banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,6 +5,10 @@ import Baanner from "../img/banner.jpeg";
 
 const Banner = () => {
   const classes = useStyles();
+
+  const truncate = (string, n) => string?.length > n ? `${string.substr(0, n-1)} ...` : string
+    
+
   return (
     <div className={classes.root}>
       <div className={classes.content}>
@@ -14,6 +18,12 @@ const Banner = () => {
         <div className={classes.buttons}>
         <Button>Play</Button>
         <Button>My List</Button>
+        <Typography style={{wordWrap: 'break-word'}} variant="h6" className={classes.description}>
+          {
+            truncate('          Movie Description Movie Description Movie Description Movie Description Movie Description Movie Description Movie Description Movie Description Movie Description Movie Description Movie Description Movie Description ', 160)
+          }
+        </Typography>
+        <div className={classes.fadeBottom}></div>
         </div>
       </div>
     </div>
@@ -44,6 +54,15 @@ const useStyles = makeStyles((theme) => ({
       color: '#000',
       backgroundColor: '#e6e6e6'
     }
+  },
+  fadeBottom: {
+    position: 'absolute',
+    top: '30vh',
+    bottom: '0',
+    left: '0',
+    right: '0',
+    zIndex: 99,
+    backgroundImage: 'linear-gradient(160deg, transparent, rgba(37,37,37,0.61), #111)'
   }
 }));
 
