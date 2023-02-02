a163979f9bddbc513e0cd68b75b51bcb0ea3c22c
EcheCostabel <exequielcostabel@gmail.com>
2023-02-02 16:35:25 -0300

    profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,38 +1,42 @@
-import { Typography } from '@mui/material';
-import { makeStyles } from '@mui/styles';
-import React from 'react';
-import Header from '../components/Header';
-import Plans from '../components/Plans';
-import avatar from '../img/img-avatar.jpg';
-import { NetflixButton } from '../styled/styledcomponents';
+import { Typography } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import React from "react";
+import Header from "../components/Header";
+import Plans from "../components/Plans";
+import avatar from "../img/img-avatar.jpg";
+import { NetflixButton } from "../styled/styledcomponents";
 
 const Profile = () => {
   const classes = useStyles();
   return (
     <div className={classes.root}>
       <Header />
-      <Typography variant='h3'>Edit profile</Typography>
-      <div className={classes.info}>
-        <img src={avatar} alt='avatar' />
-        <div className={classes.details}>
-          <div className={classes.plans}>
-            <Typography variant='h6'>email usuario</Typography>
-            <Typography variant='h5' gutterBottom>Plans</Typography>
-            <Plans cost={7.99}>Netflix Standard</Plans>
-            <Plans cost={11.99}>Netflix Basic</Plans>
-            <Plans cost={15.99}>Netflix Premium</Plans>
-            <NetflixButton>Sign Out</NetflixButton>
+      <Typography variant="h3">Edit profile</Typography>
+      <div className={classes.body}>
+        <div className={classes.info}>
+          <img src={avatar} alt="avatar" />
+          <div className={classes.details}>
+            <div className={classes.plans}>
+              <Typography variant="h6">email usuario</Typography>
+              <Typography variant="h5" gutterBottom>Plans</Typography>
+              <Plans cost={7.99}>Netflix Standard</Plans>
+              <Plans cost={11.99}>Netflix Basic</Plans>
+              <Plans cost={15.99}>Netflix Premium</Plans>
+              <NetflixButton>Sign Out</NetflixButton>
+            </div>
           </div>
         </div>
       </div>
-      </div>
-  )
-}
+    </div>
+  );
+};
 
 const useStyles = makeStyles((theme) => ({
   root: {
-
-  }
+    color: "#fff",
+    minHeight: "100vh",
+    width: "100vh",
+  },
 }));
 
-export default Profile
+export default Profile;
