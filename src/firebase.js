f070f6757fbff5f22c4f7bb72acac617841e1626
EcheCostabel <exequielcostabel@gmail.com>
2023-02-06 14:18:01 -0300

    adding firebase

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAzUe_OB3Rv1KgPBkaWfUbxMrZILulEric",
+    authDomain: "netflix-clone-4595d.firebaseapp.com",
+    projectId: "netflix-clone-4595d",
+    storageBucket: "netflix-clone-4595d.appspot.com",
+    messagingSenderId: "1010207441278",
+    appId: "1:1010207441278:web:c14f0ab40447f72f25732f"
+  };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+  const auth = firebase.auth();
+
+  export {auth};
