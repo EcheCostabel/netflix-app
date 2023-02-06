b7d893240fc8a4947f052919ca80a1dacb307512
EcheCostabel <exequielcostabel@gmail.com>
2023-02-06 17:04:22 -0300

    llenando row

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,14 +1,27 @@
 import { Typography } from '@mui/material';
 import { makeStyles } from '@mui/styles';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Rows = ({title, fetchUrl, isLargeRow }) => {
   const classes = useStyles();
+  const [movies, setMovies ] = useState();
+
+  const base_url = 'https://image.tmdb.org/t/p/original'
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const request = await axios.get(fetchUrl)
+      setMovies(request.data.results)
+      return request
+    }
+    fetchData()
+  },[fetchUrl])
+
   return (
     <div className={classes.root}>
       <Typography variant='h4'>{title}</Typography>
       <div className={classes.posters}>
-        
+
       </div>
       </div>
   )
