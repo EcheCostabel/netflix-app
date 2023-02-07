3f6164c3e3951660d1bf0b14ffacfd9a516d0e81
EcheCostabel <exequielcostabel@gmail.com>
2023-02-07 19:15:18 -0300

    finishhh

--- src/components/Plans.js
+++ src/components/Plans.js
@@ -1,18 +1,33 @@
 import { Typography } from '@mui/material';
 import { makeStyles } from '@mui/styles';
 import React, { useState } from 'react'
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { setPrice } from '../features/PriceSlice';
 import { NetflixButton } from '../styled/styledcomponents';
 
 const Plans = ({cost, children, color, wide}) => {
-
-
+  const dispatch = useDispatch()
   const classes = useStyles();
+  const navigate = useNavigate()
+
+  const handleClick = (cost) => {
+    dispatch(setPrice(cost))
+    navigate('/checkout')
+  }
+
   return (
       <div className={classes.root}>
         <Typography className={classes.standard} variant='h5'>
             {children}
         </Typography>
-        <NetflixButton color={color} wide={wide}>Subscribe</NetflixButton>
+        <NetflixButton
+        color={color} 
+        wide={wide}
+        onClick={()=> handleClick(cost)}
+        >
+          Subscribe
+        </NetflixButton>
       </div>
   )
 }
--- src/pages/Paypal.js
+++ src/pages/Paypal.js
@@ -1,17 +1,45 @@
-import { makeStyles } from '@mui/styles';
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+import { useSelector } from 'react-redux';
+import { selectPrice } from '../features/PriceSlice';
+
+
 
 const Paypal = () => {
-    const classes = useStyles();
+    const price = useSelector(selectPrice)
+    const paypal = useRef();
+
+    useEffect(() => {
+      window.paypal
+      .Buttons({
+        style: {
+          tagline: false,
+      },
+        createOrder: (data, actions, err)=> {
+          return actions.order.create({
+            intent: 'CAPTURE',
+            purchase_units: [
+              {
+                description: 'Netflix Subscription',
+                amount: {
+                  currency_code: 'EUR',
+                  value: price
+                }
+              }
+            ]
+          })
+        },
+        onApprove: async (data, actions)=> {
+          const order = await actions.order.capture();
+          console.log(order)
+        },
+        onError: err => console.log(err)
+      }).render(paypal.current)
+    },[])
   return (
-    <div>Paypal</div>
+    <div ref={paypal}></div>
   )
 }
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-  
-    }
-  }));
 
 export default Paypal
+
